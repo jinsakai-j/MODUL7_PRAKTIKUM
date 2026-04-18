715d35060e48130c50fab7b693b333c4fcff9576
NIGGA BUT TOKEN <nigga22@gmail.com>
2026-04-18 08:51:26 +0700

    FilmFavorit_103022400035

--- MODUL7_103022400116/Program.cs
+++ MODUL7_103022400116/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MODUL7_103022400116;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main() {
+        FilmFavorit_103022400035 films = new FilmFavorit_103022400035();
+        films.ReadJSON();
+    }
+}
+
+
