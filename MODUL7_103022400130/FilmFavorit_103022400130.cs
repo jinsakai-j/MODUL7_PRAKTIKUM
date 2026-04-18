aa839e06b0c348a93270d1a9502664f9ff226b24
niggerbutt <niggerbutt@gmail.com>
2026-04-18 09:09:20 +0700

    Menambhkan class FilmFavorit

---
+++ MODUL7_103022400130/FilmFavorit_103022400130.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace MODUL7_103022400116
+{
+    internal class FilmFavorit_103022400130
+    {
+        public string title {  get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public Boolean isWatched { get; set; }
+
+        public void ReadJSON()
+        {
+            try
+            {
+                string filePath = "jurnal7_1_103022400130.json";
+                string jsonString = File.ReadAllText(filePath);
+
+                FilmFavorit_103022400130 film = JsonSerializer.Deserialize<FilmFavorit_103022400130>(jsonString);
+
+                Console.WriteLine("Watchlist Name : " + film.title);
+                Console.WriteLine("Creaetd By : " + film.director);
+                Console.WriteLine("Year : " + film.year);
+                Console.WriteLine("Genre : " + film.genre);
+                Console.WriteLine("Rating : " + film.rating);
+                Console.WriteLine("Durasi : " + film.durationMinutes);
+                Console.WriteLine("Is Watched : " + film.isWatched);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saat membaca JSON: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ MODUL7_103022400130/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MODUL7_103022400116
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FilmFavorit_103022400130 film = new FilmFavorit_103022400130();
+
+            film.ReadJSON();
+
+            Console.WriteLine();
+        }
+    }
+}
